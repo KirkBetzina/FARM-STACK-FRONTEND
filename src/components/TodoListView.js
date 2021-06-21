8ba1546fad6ecc50454948fc860995db3bfb2d77
kirk <kirk.betzina@gmail.com>
2021-06-21 09:27:39 -0400

    remade git repository, commiting and troubleshooting delete route

---
+++ src/components/TodoListView.js
@@ -0,0 +1,14 @@
+import TodoItem from "./Todo";
+
+function TodoView(props) {
+    console.log('HEY OVER HERE', props)
+    return ( 
+        <div>
+            <ul>
+                {props.todoList.map((todo, idx) => <TodoItem todo ={todo} key ={idx}/>)}
+                
+            </ul>
+        </div>
+    )
+}
+export default TodoView
